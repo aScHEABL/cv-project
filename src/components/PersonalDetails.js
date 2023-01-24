32aff01c5157343b6667b2977a4545218e83e471
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-01-24 11:21:50 +0800

    update: Personal Details grid #0

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -8,9 +8,9 @@ class PersonalDetails extends Component {
 
     render() {
         return (
-        <section className="h-1/2 grid">
-            <h2 className="text-slate-100 text-2xl">Personal Details</h2>
-            <br />
+        <section className="h-1/2 grid grid-cols-[2fr_2fr_2fr_2fr_2fr] 
+        grid-rows-[1fr_1fr_1fr_1fr_1fr] ">
+            <h2 className="text-slate-100 text-2xl col-span-5">Personal Details</h2>
             <div className="flex gap-4 h-fit w-fit">
                 <Photo />
             </div>
