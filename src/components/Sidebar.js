4f909af88844f76d6f78a129d078a0ba9bb515fc
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-04 17:30:27 +0800

    update: PersonalDetails state WIP #0

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -28,9 +28,9 @@ class Sidebar extends Component {
                             
                         </div>
                         <div className="flex text-white text-[10px] gap-2 tracking-wide font-work-sans font-normal text-[#a6a6a6]">
-                            <svg style={iconStyle} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                            <svg style={iconStyle} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                             </svg>
                             
                         </div>
