fd94e33296c6ab5fe68fa345f92e586300817ed6
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-14 23:45:29 +0800

    fix: PersonalDetails input text is white

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -3,44 +3,25 @@ import React, { Component } from "react";
 class PersonalDetails extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            firstName: '',
-            lastName: '',
-            address: '',
-            website: '',
-            email: '',
-            phoneNumber: '',
-            introduction: '',
-
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        this.setState({ 
-            [e.target.name]: e.target.value
-        })
     }
 
     render() {
         return(
             <div className="py-4 px-8 grid grid-cols-[1fr_1fr] gap-3 bg-component-gray h-fit w-full rounded-xl">
                 <h2 className="h-9 col-span-2 text-white text-2xl">Personal Details</h2>
-                <input className="h-12 bg-input-box-gray rounded px-3" placeholder="First Name" 
+                <input className="h-12 bg-input-box-gray rounded px-3 text-white" placeholder="First Name" 
                 onChange={this.handleChange} name="firstName" />
                 <input className="h-12 bg-input-box-gray rounded px-3" placeholder="Last Name" 
                 onChange={this.handleChange} name="lastName" />
-                <input className="h-12 bg-input-box-gray rounded px-3 col-span-2" placeholder="Address" 
+                <input className="h-12 bg-input-box-gray rounded px-3 col-span-2 text-white" placeholder="Address" 
                 onChange={this.handleChange} name="address" />
-                <input className="h-12 bg-input-box-gray rounded px-3 col-span-2" placeholder="Website" 
+                <input className="h-12 bg-input-box-gray rounded px-3 col-span-2 text-white" placeholder="Website" 
                 onChange={this.handleChange} name="website" />
-                <input className="h-12 bg-input-box-gray rounded px-3" placeholder="Email" 
+                <input className="h-12 bg-input-box-gray rounded px-3 text-white" placeholder="Email" 
                 onChange={this.handleChange} name="email" />
-                <input className="h-12 bg-input-box-gray rounded px-3" placeholder="Phone Number" 
+                <input className="h-12 bg-input-box-gray rounded px-3 text-white" placeholder="Phone Number" 
                 onChange={this.handleChange} name="phoneNumber" />
-                <textarea className="h-32 col-span-2 bg-input-box-gray rounded px-3 py-3" placeholder="Tell something about yourself!" 
+                <textarea className="h-32 col-span-2 bg-input-box-gray rounded px-3 py-3 text-white" placeholder="Tell something about yourself!" 
                 onChange={this.handleChange} name="introduction" />
             </div>
         )
