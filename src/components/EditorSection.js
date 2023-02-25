4521835999c32fe45b3e4ded2bf145bde4c14660
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-25 10:20:03 +0800

    update: update import path AppTitle.js

--- src/components/EditorSection.js
+++ src/components/EditorSection.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CVcreator from "./CVcreator";
+import CVcreator from "./AppTitle";
 import PersonalDetails from "./PersonalDetails";
 import Education from "./Education";
 import Employment from "./Employment";
 import Skill from "./Skill";
 
 const EditorSection = () => {
-    
+
     return (
         <section className="flex flex-wrap flex-col gap-8 h-fit shrink-0">
             <CVcreator />
