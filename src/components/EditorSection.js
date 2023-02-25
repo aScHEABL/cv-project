13472dad82174e50c5c05f4d02f047bcbdf7b4ac
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-25 10:18:23 +0800

    update: refactor to functional component

--- src/components/EditorSection.js
+++ src/components/EditorSection.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import CVcreator from "./CVcreator";
 import PersonalDetails from "./PersonalDetails";
 import Education from "./Education";
 import Employment from "./Employment";
 import Skill from "./Skill";
 
-class EditorSection extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return(
-            <section className="flex flex-wrap flex-col gap-8 h-fit shrink-0">
-                <CVcreator />
-                <PersonalDetails />
-                <Employment />
-                <Education />
-                <Skill />
-            </section>
-        )
-    }
+const EditorSection = () => {
+    
+    return (
+        <section className="flex flex-wrap flex-col gap-8 h-fit shrink-0">
+            <CVcreator />
+            <PersonalDetails />
+            <Employment />
+            <Education />
+            <Skill />
+        </section>
+    )
 }
 
 export default EditorSection;
