86d9d558deab63a285929908d1b2de8e571c5b52
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-25 10:18:28 +0800

    update: refactor to functional component

--- src/components/PreviewSection.js
+++ src/components/PreviewSection.js
@@ -1,24 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./Header";
 import CurrentPos from "./CurrentPos";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 
-class PreviewSection extends Component {
-    constructor(props) {
-        super(props);
-    }
+const PreviewSection = () => {
 
-    render() {
-        return(
-            <section className="grid grid-rows-[0.8fr_0.25fr_5fr] grid-cols-[3.5fr_7fr] h-preview-section w-preview-section sticky top-5 bottom-5 bg-white rounded-lg shrink-0">
-                <Header />
-                <CurrentPos />
-                <Sidebar />
-                <Content />
-            </section>
-        )
-    }
+    return(
+        <section className="grid grid-rows-[0.8fr_0.25fr_5fr] grid-cols-[3.5fr_7fr] h-preview-section w-preview-section sticky top-5 bottom-5 bg-white rounded-lg shrink-0">
+            <Header />
+            <CurrentPos />
+            <Sidebar />
+            <Content />
+        </section>
+    )
 }
 
 export default PreviewSection;
