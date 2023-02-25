0a5e87d4a4bb4179e860630a844a522798ef1c6e
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-25 08:09:36 +0800

    A new branch written in functional components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import EditorSection from "./components/EditorSection";
 import PreviewSetion from "./components/PreviewSection";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      PD: {
-        firstName: '',
-        lastName: '',
-        address: '',
-        website: '',
-        email: '',
-        phoneNumber: '',
-        intro: '',
-      },
-
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    
-  }
-
-  handleChange(e) {
-
-  }
-
-  render() {
-    return (
-      <main className="px-10 py-5 bg-black flex justify-between items-start h-max gap-32">
-        <EditorSection context={this.state} />
-        <PreviewSetion context={this.state} />
-        <div className="flex-1"></div>
-      </main>
-    )
-  }
+const App = () => {
+  return (
+    <main className="px-10 py-5 bg-black flex justify-between items-start h-max gap-32">
+      <EditorSection />
+      <PreviewSetion />
+      <div className="flex-1"></div>
+    </main>
+  )
 }
 
 export default App;
