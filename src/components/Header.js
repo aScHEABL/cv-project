69819c0b6ee88f78025a674a35210b6dd7048323
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-02-25 12:26:57 +0800

    update: refactor to functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return(
-            <div className="col-span-2 flex justify-center items-center gap-8">
-                <h1 className="text-4xl font-work-sans font-medium tracking-[0.3em]"></h1>
-                <h1 className="text-4xl font-work-sans font-light tracking-[0.3em]"></h1>
-            </div>
-        )
-    }
+const Header = () => {
+    
+    return(
+        <div className="col-span-2 flex justify-center items-center gap-8">
+            <h1 className="text-4xl font-work-sans font-medium tracking-[0.3em]"></h1>
+            <h1 className="text-4xl font-work-sans font-light tracking-[0.3em]"></h1>
+        </div>
+    )
 }
 
 export default Header;
