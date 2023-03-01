38e42611057178fd953502ca0f94b996047e835d
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-01 10:46:21 +0800

    fix: input text isn't not white

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useEffect }  from "react";
+import { useCvContext } from "../context/cvContext";
 
 const Content = () => {
-    
+    const [cvState] = useCvContext();
+    useEffect(() => {}, [cvState]);
     return (
         <div className="flex flex-col p-6">
             <div className="flex flex-wrap gap-3 pb-8">
                 <h3 className="w-full font-work-sans text-[13px] tracking-[4px]">PROFILE</h3>
                 <div className="h-0.5 w-16 bg-component-gray" />
+                <div className="h-0 w-full"></div>
                 <p className="font-work-sans text-[11px] font-normal text-[#2e2e2e] ">
-                    
+                    {cvState.intro}
                 </p>
             </div>
             <div className="flex flex-wrap gap-3 pb-8">
                 <h3 className="w-full font-work-sans text-[13px] tracking-[4px]">WORK EXPERIENCE</h3>
                 <div className="h-0.5 w-16 bg-component-gray" />
+                <div className="h-0 w-full"></div>
                 <div className="flex w-full flex-wrap gap-0.5">
                     <h4 className="w-full font-work-sans text-xs font-semibold"></h4>
                     <h5 className="text-[11px]"></h5>
@@ -21,7 +25,7 @@ const Content = () => {
                     <h5 className="text-[11px]"></h5>
                 </div>
                 <p className="font-work-sans text-[11px] font-normal text-[#2e2e2e]">
-                
+                    
                 </p>
             </div>
             <div className="flex flex-wrap gap-3">
