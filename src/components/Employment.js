eb46445f2bab8d83965d887d7aa9f5e199f00ef0
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-07 15:57:11 +0800

    update: add new states to context

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useCvContext } from "../context/cvContext";
 
 const Employment = () => {
+    const [cvState, dispatch] = useCvContext();
+    const handleChange = (key, value) => {
+        dispatch({ type: `${key}`, payload: { [key]: value } });
+    }
+
     
     return(
         <div className="py-4 px-8 gap-3 grid grid-cols-[1fr_1fr] 
@@ -8,37 +14,50 @@ const Employment = () => {
             <h2 className="h-9 col-span-2 text-white text-2xl">Employment History</h2>
             <label className="text-white col-span-2">Work Experience #1</label>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Job Title" />
+            placeholder="Job Title" name="jobTitle_first" value={cvState.jobTitle_first}
+            onChange={(e) => handleChange('jobTitle_first', e.target.value)} />
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Employer" />
+            placeholder="Employer" name="employer_first" value={cvState.employer_first}
+            onChange={(e) => handleChange('employer_first', e.target.value)} />
             <div className="flex w-full h-fit gap-3 justify-between items-center">
                 <input className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" />
+                placeholder="MM/YYYY" name="startDate_first" value={cvState.startDate_first}
+                onChange={(e) => handleChange('startDate_first', e.target.value)} />
                 <label className="text-white">to</label>
                 <input className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" />
+                placeholder="MM/YYYY" name="endDate_first" value={cvState.endDate_first}
+                onChange={(e) => handleChange('endDate_first', e.target.value)} />
             </div>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="City" />
+            placeholder="City" name="city_first" value={cvState.city_first}
+            onChange={(e) => handleChange('city_first', e.target.value)} />
             <textarea className="h-32 col-span-2 bg-input-box-gray rounded 
             px-3 py-3 text-white" 
-            placeholder="Write some details about your previous employment!" />
+            placeholder="Write some details about your previous employment!" 
+            name="jobDesc_first" value={cvState.jobDesc_first}
+            onChange={(e) => handleChange('jobDesc_first', e.target.value)} />
             <br />
             <label className="text-white col-span-2">Work Experience #2</label>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Job Title" />
+            placeholder="Job Title" name="jobTitle_second" value={cvState.jobTitle_second}
+            onChange={(e) => handleChange('jobTitle_second', e.target.value)} />
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Employer" />
+            placeholder="Employer" name="employer_second" value={cvState.employer_second}
+            onChange={(e) => handleChange('employer_second', e.target.value)} />
             <div className="flex w-full h-fit gap-3 justify-between items-center">
                 <input className="bg-input-box-gray w-28 h-12 rounded px-3 text-white"
-                placeholder="MM/YYYY" />
+                placeholder="MM/YYYY" name="startDate_second" value={cvState.startDate_second}
+                onChange={(e) => handleChange('startDate_second', e.target.value)} />
                 <label className="text-white">to</label>
                 <input className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" />
+                placeholder="MM/YYYY" name="endDate_second" value={cvState.endDate_second}
+                onChange={(e) => handleChange('endDate_second', e.target.value)} />
             </div>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" placeholder="City" />
             <textarea className="h-32 col-span-2 text-white bg-input-box-gray rounded px-3 py-3"
-            placeholder="Write some details about your previous employment!" />
+            placeholder="Write some details about your previous employment!"
+            name="jobDesc_second" value={cvState.jobDesc_second}
+            onChange={(e) => handleChange('jobDesc_second', e.target.value)} />
         </div>
     )
 }
--- src/context/cvContext.js
+++ src/context/cvContext.js
@@ -43,6 +43,66 @@ const reducer = (state, action) => {
                 ...state,
                 intro: action.payload.intro
             }
+        case 'jobTitle_first':
+            return {
+                ...state,
+                intro: action.payload.jobTitle_first
+            }
+        case 'jobTitle_second':
+            return {
+                ...state,
+                jobTitle_second: action.payload.jobDesc_second
+            }
+        case 'employer_first':
+            return {
+                ...state,
+                employer_first: action.payload.employer_first
+            }
+        case 'employer_second':
+            return {
+                ...state,
+                employer_second: action.payload.employer_second
+            }
+        case 'startDate_first':
+            return {
+                ...state,
+                startDate_first: action.payload.startDate_first
+            }
+        case 'startDate_second':
+            return {
+                ...state,
+                startDate_second: action.payload.startDate_second
+            }
+        case 'endDate_first':
+            return {
+                ...state,
+                endDate_first: action.payload.endDate_first
+            }
+        case 'endDate_second':
+            return {
+                ...state,
+                endDate_second: action.payload.endDate_second
+            }
+        case 'city_first':
+            return {
+                ...state,
+                city_first: action.payload.city_first
+            }
+        case 'city_second':
+            return {
+                ...state,
+                city_second: action.payload.city_second
+            }
+        case 'jobDesc_first':
+            return {
+                ...state,
+                jobDesc_first: action.payload.jobDesc_first
+            }
+        case 'jobDesc_second':
+            return {
+                ...state,
+                jobDesc_second: action.payload.jobDesc_second
+            }
         default:
             return state;
     }
@@ -56,7 +116,19 @@ const CvContextProvider = ( {children }) => {
         website: '',
         email: '',
         phone: '',
-        intro: ''
+        intro: '',
+        jobTitle_first: '',
+        employer_first: '',
+        startDate_first: '',
+        endDate_first: '',
+        city_first: '',
+        jobDesc_first: '',
+        jobTitle_second: '',
+        employer_second: '',
+        startDate_second: '',
+        endDate_second: '',
+        city_second: '',
+        jobDesc_second: '',
     })
     return (
         <cvContext.Provider value={valueAndDispatcher}>
