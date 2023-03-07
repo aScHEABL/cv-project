fb0ff173c8e1d300e96acf3fc939e12853d57b6a
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-07 16:50:29 +0800

    update: render states to dom

--- src/components/Content.js
+++ src/components/Content.js
@@ -19,24 +19,24 @@ const Content = () => {
                 <div className="h-0.5 w-16 bg-component-gray" />
                 <div className="h-0 w-full"></div>
                 <div className="flex w-full flex-wrap gap-0.5">
-                    <h4 className="w-full font-work-sans text-xs font-semibold"></h4>
-                    <h5 className="text-[11px]"></h5>
-                    <label className="text-[11px]">  </label>
-                    <h5 className="text-[11px]"></h5>
+                    <h4 className="w-full font-work-sans text-xs font-semibold">{cvState.jobTitle_first}</h4>
+                    <h5 className="text-[11px]">{cvState.startDate_first}</h5>
+                    <label className="text-[11px]"> - </label>
+                    <h5 className="text-[11px]">{cvState.endDate_first}</h5>
                 </div>
                 <p className="font-work-sans text-[11px] font-normal text-[#2e2e2e]">
-                    
+                    {cvState.jobDesc_first}
                 </p>
             </div>
             <div className="flex flex-wrap gap-3">
                 <div className="flex w-full flex-wrap gap-0.5">
-                    <h4 className="w-full font-work-sans text-xs font-semibold"></h4>
-                    <h5 className="text-[11px]"></h5>
-                    <label className="text-[11px]"></label>
-                    <h5 className="text-[11px]"></h5>
+                    <h4 className="w-full font-work-sans text-xs font-semibold">{cvState.jobTitle_second}</h4>
+                    <h5 className="text-[11px]">{cvState.startDate_second}</h5>
+                    <label className="text-[11px]"> - </label>
+                    <h5 className="text-[11px]">{cvState.endDate_second}</h5>
                 </div>
                 <p className="font-work-sans text-[11px] font-normal text-[#2e2e2e]">
-                    
+                    {cvState.jobDesc_second}
                 </p>
             </div>
         </div>
