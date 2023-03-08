0b20aa1a6771b8a5967a371b34f7167c6462b6fb
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-08 16:46:56 +0800

    update: state to context

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,25 +1,36 @@
 import React from "react";
+import { useCvContext } from "../context/cvContext";
 
 const Education = () => {
-    
+    const [cvState, dispatch] = useCvContext();
+    const handleChange = (key, value) => {
+        dispatch({ type: `${key}`, payload: { [key]: value } });
+    }
+
     return (
         <div className="py-4 px-8 gap-3 grid grid-cols-[1fr_1fr] bg-component-gray
         h-fit w-full rounded-xl">
             <h2 className="h-9 col-span-2 text-white text-2xl">Education</h2>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white"
-            placeholder="School" />
+            placeholder="School" 
+            name="school" value={cvState.school} 
+            onChange={(e) => handleChange(e.target.name, e.target.value)} />
             <input className="bg-input-box-gray h-12 rounded px-3 text-white"
-            placeholder="Degree" />
+            placeholder="Degree" name="degree" value={cvState.degree}
+            onChange={(e) => handleChange(e.target.name, e.target.value)} />
             <div className="flex w-full h-fit gap-3 justify-between items-center">
                 {/* <label className="text-white w-full">Start & End Date</label> */}
-                <input className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" />
+                <input type="date" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
+                placeholder="MM/YYYY" name="eduStartDate" value={cvState.eduStartDate}
+                onChange={(e) => handleChange(e.target.name, e.target.value)} />
                 <label className="text-white">to</label>
-                <input className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" />
+                <input type="date" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
+                placeholder="MM/YYYY" name="eduEndDate" value={cvState.eduEndDate}
+                onChange={(e) => handleChange(e.target.name, e.target.value)} />
             </div>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="City" />
+            placeholder="City" name="eduCity" value={cvState.eduCity}
+            onChange={(e) => handleChange(e.target.name, e.target.value)} />
         </div>
     )
 }
