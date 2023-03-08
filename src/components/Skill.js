a30776b62fd42d9c3517f8672c1f0eeb23b5e65e
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-08 16:47:03 +0800

    update: states to context

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useCvContext } from "../context/cvContext";
 
 const Skill = () => {
+    const [cvState, dispatch] = useCvContext();
+    const handleChange = (key, value) => {
+        dispatch({ type: `${key}`, payload: { [key]: value } });
+    }
     
     const svgStyle = {
         filter: 'invert()',
@@ -12,7 +17,8 @@ const Skill = () => {
             <h2 className="h-9 col-span-2 text-white text-2xl">Skills</h2>
             <div className="flex items-center gap-3">
                 <input className="bg-input-box-gray h-12 w-full rounded px-3 text-white" 
-                placeholder="React" />
+                placeholder="React" name="skill_1" value={cvState.skill_1}
+                onChange={(e) => handleChange(e.target.name, e.target.value)} />
                 <button className="pr-3">
                 <svg style={svgStyle} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -21,7 +27,8 @@ const Skill = () => {
             </div>
             <div className="flex items-center gap-3">
                 <input className="bg-input-box-gray h-12 w-full rounded px-3 text-white" 
-                placeholder="Node.js" />
+                placeholder="Node.js" name="skill_2" value={cvState.skill_2}
+                onChange={(e) => handleChange(e.target.name, e.target.value)} />
                 <button className="pr-3">
                 <svg style={svgStyle} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -30,7 +37,8 @@ const Skill = () => {
             </div>
             <div className="flex items-center gap-3">
                 <input className="bg-input-box-gray h-12 w-full rounded px-3 text-white" 
-                placeholder="PostgreSQL" />
+                placeholder="PostgreSQL" name="skill_3" value={cvState.skill_3}
+                onChange={(e) => handleChange(e.target.name, e.target.value)} />
                 <button className="pr-3">
                 <svg style={svgStyle} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
