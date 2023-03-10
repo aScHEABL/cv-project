44f5420f3b1c2959befe0f7c3c04ff8e75d90a06
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-10 17:04:48 +0800

    update: context refactor WIP#0

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -3,8 +3,17 @@ import { useCvContext } from "../context/cvContext";
 
 const Employment = () => {
     const [cvState, dispatch] = useCvContext();
-    const handleChange = (key, value) => {
-        dispatch({ type: `${key}`, payload: { [key]: value } });
+    const handleChange = (group, name, value) => {
+        // class Job {
+        //     constructor(title, employer, startDate, endDate, desc) {
+        //         Object.assign(this, {title, employer, startDate, endDate, desc})
+        //     }
+        // }
+        const firstJob = {};
+        const secondJob = {};
+        if (group === "first") Object.defineProperty(firstJob, [name], { value: value });
+        else Object.defineProperty(secondJob, [name], { value: value });
+        dispatch({type: "ADD_JOB", payload: })
     }
 
     
@@ -14,44 +23,35 @@ const Employment = () => {
             <h2 className="h-9 col-span-2 text-white text-2xl">Employment History</h2>
             <label className="text-white col-span-2">Work Experience #1</label>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Job Title" name="jobTitle_first" value={cvState.jobTitle_first}
-            onChange={(e) => handleChange('jobTitle_first', e.target.value)} />
+            placeholder="Job Title" group="first" name="title" value={cvState.jobs[0].title}
+            onChange={(e) => handleChange(e.target.group, e.target.name, e.target.value)} />
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Employer" name="employer_first" value={cvState.employer_first}
-            onChange={(e) => handleChange('employer_first', e.target.value)} />
+            placeholder="Employer" name="employer" value={cvState.jobs[0].employer} />
             <div className="flex w-full h-fit gap-3 justify-between items-center">
-                <input type="date" date-date-format="MM YYYY" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" name="startDate_first" value={cvState.startDate_first}
-                onChange={(e) => handleChange('startDate_first', e.target.value)} />
+                <input type="date" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
+                placeholder="MM/YYYY" name="startDate" value={cvState.jobs[0].startDate} />
                 <label className="text-white">to</label>
-                <input type="date" date-date-format="MM YYYY" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" name="endDate_first" value={cvState.endDate_first}
-                onChange={(e) => handleChange('endDate_first', e.target.value)} />
+                <input type="date" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
+                placeholder="MM/YYYY" name="endDate" value={cvState.jobs[0].endDate} />
             </div>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="City" name="city_first" value={cvState.city_first}
-            onChange={(e) => handleChange('city_first', e.target.value)} />
+            placeholder="City" name="city" value={cvState.jobs[0].city} />
             <textarea className="h-32 col-span-2 bg-input-box-gray rounded 
             px-3 py-3 text-white" 
             placeholder="Write some details about your previous employment!" 
-            name="jobDesc_first" value={cvState.jobDesc_first}
-            onChange={(e) => handleChange('jobDesc_first', e.target.value)} />
+            name="desc" value={cvState.jobs[0].desc} />
             <br />
             <label className="text-white col-span-2">Work Experience #2</label>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Job Title" name="jobTitle_second" value={cvState.jobTitle_second}
-            onChange={(e) => handleChange('jobTitle_second', e.target.value)} />
+            placeholder="Job Title" name="jobTitle_second" />
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="Employer" name="employer_second" value={cvState.employer_second}
-            onChange={(e) => handleChange('employer_second', e.target.value)} />
+            placeholder="Employer" name="employer_second" />
             <div className="flex w-full h-fit gap-3 justify-between items-center">
                 <input type="date" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white"
-                placeholder="MM/YYYY" name="startDate_second" value={cvState.startDate_second}
-                onChange={(e) => handleChange('startDate_second', e.target.value)} />
+                placeholder="MM/YYYY" name="startDate_second" />
                 <label className="text-white">to</label>
                 <input type="date" className="bg-input-box-gray w-28 h-12 rounded px-3 text-white" 
-                placeholder="MM/YYYY" name="endDate_second" value={cvState.endDate_second}
-                onChange={(e) => handleChange('endDate_second', e.target.value)} />
+                placeholder="MM/YYYY" name="endDate_second" />
             </div>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" placeholder="City" />
             <textarea className="h-32 col-span-2 text-white bg-input-box-gray rounded px-3 py-3"
