d855271e45bf05b148cb81770c06ca0defc8d05a
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-12 20:55:32 +0800

    update: state with array of objects

--- src/context/cvContext.js
+++ src/context/cvContext.js
@@ -43,105 +43,21 @@ const reducer = (state, action) => {
                 ...state,
                 intro: action.payload.intro
             }
-        case 'jobTitle_first':
+        case 'ADD_JOB':
             return {
                 ...state,
-                jobTitle_first: action.payload.jobTitle_first
+                jobs: [
+                    ...state.jobs,
+                    action.payload.jobs
+                ]
             }
-        case 'jobTitle_second':
+        case 'ADD_SKILL':
             return {
                 ...state,
-                jobTitle_second: action.payload.jobTitle_second
-            }
-        case 'employer_first':
-            return {
-                ...state,
-                employer_first: action.payload.employer_first
-            }
-        case 'employer_second':
-            return {
-                ...state,
-                employer_second: action.payload.employer_second
-            }
-        case 'startDate_first':
-            return {
-                ...state,
-                startDate_first: action.payload.startDate_first
-            }
-        case 'startDate_second':
-            return {
-                ...state,
-                startDate_second: action.payload.startDate_second
-            }
-        case 'endDate_first':
-            return {
-                ...state,
-                endDate_first: action.payload.endDate_first
-            }
-        case 'endDate_second':
-            return {
-                ...state,
-                endDate_second: action.payload.endDate_second
-            }
-        case 'city_first':
-            return {
-                ...state,
-                city_first: action.payload.city_first
-            }
-        case 'city_second':
-            return {
-                ...state,
-                city_second: action.payload.city_second
-            }
-        case 'jobDesc_first':
-            return {
-                ...state,
-                jobDesc_first: action.payload.jobDesc_first
-            }
-        case 'jobDesc_second':
-            return {
-                ...state,
-                jobDesc_second: action.payload.jobDesc_second
-            }
-        case 'school':
-            return {
-                ...state,
-                school: action.payload.school
-            }
-        case 'degree':
-            return {
-                ...state,
-                degree: action.payload.degree
-            }
-        case 'eduStartDate':
-            return {
-                ...state,
-                eduStartDate: action.payload.eduStartDate
-            }
-        case 'eduEndDate':
-            return {
-                ...state,
-                eduEndDate: action.payload.eduEndDate
-            }
-        case 'eduCity':
-            return {
-                ...state,
-                eduCity: action.payload.eduCity
-            }
-        case 'skill_1':
-            return {
-                ...state,
-                skill_1: action.payload.skill_1
-            }
-        case 'skill_2':
-            return {
-                ...state,
-                skill_2: action.payload.skill_2
-            }
-        case 'skill_3':
-            return {
-                ...state,
-                skill_3: action.payload.skill_3
+                skills: [
+                    ...state.skills,
+                    action.payload.skills
+                ]
             }
         default:
             return state;
@@ -157,26 +73,8 @@ const CvContextProvider = ( { children }) => {
         email: '',
         phone: '',
         intro: '',
-        jobTitle_first: '',
-        employer_first: '',
-        startDate_first: '',
-        endDate_first: '',
-        city_first: '',
-        jobDesc_first: '',
-        jobTitle_second: '',
-        employer_second: '',
-        startDate_second: '',
-        endDate_second: '',
-        city_second: '',
-        jobDesc_second: '',
-        school: '',
-        degree: '',
-        eduStartDate: '',
-        eduEndDate: '',
-        eduCity: '',
-        skill_1: '',
-        skill_2: '',
-        skill_3: ''
+        jobs: [],
+        skills: [],
     })
     return (
         <cvContext.Provider value={valueAndDispatcher}>
