0409a48a9e7ea647f22d0296defc659e74be3683
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-14 17:30:27 +0800

    update: conditionally render WIP #0

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useCvContext } from "../context/cvContext";
 import WE_LIST from "./list/WE_LIST";
 
@@ -6,13 +6,12 @@ let nextID = 0;
 
 const Employment = () => {
     const [cvState, dispatch] = useCvContext();
+
     
     const handleClick = () => {
         dispatch({
             type: 'ADD_JOB', 
-            id: nextID++, 
-            title: '',
-            employer: '',
+            id: nextID++,
          });
     }
     
@@ -21,13 +20,15 @@ const Employment = () => {
         bg-component-gray h-fit w-full rounded-xl">
             <div className="grid col-span-2 grid-cols-[8fr_2fr] items-center ">
                 <h2 className="h-9 text-white text-2xl">Employment History</h2>
-                <button className="flex gap-2 bg-mint rounded-xl p-4 text-lg items-center"
+                { buttonEnable ? <button className="flex gap-2 bg-mint rounded-xl p-4 text-lg items-center"
                 onClick={handleClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                             Add
                 </button>
+                : null
+                }
             </div>
             <WE_LIST />
         </div>
