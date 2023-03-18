ac2893747228c503b7ab31986e047044d03da6da
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-03-19 03:25:13 +0800

    update: remove console.log

--- src/components/list/WE_LIST.js
+++ src/components/list/WE_LIST.js
@@ -5,7 +5,6 @@ export default function WE_LIST() {
     const [cvState, dispatch] = useCvContext();
     const handleChange = (id, key, value) => {
         dispatch({ type: 'CHANGE_JOB', id: id, key: key, value: value })
-        console.log(cvState.jobs);
     }
     const nodes = cvState.jobs.map((job) => (
         <React.Fragment key={job.id}>
@@ -26,7 +25,7 @@ export default function WE_LIST() {
                 onChange={(e) => handleChange(job.id, e.target.name, e.target.value)} />
             </div>
             <input className="bg-input-box-gray h-12 rounded px-3 text-white" 
-            placeholder="City" name="city" value={job.city}
+            placeholder="Country" name="country" value={job.country}
             onChange={(e) => handleChange(job.id, e.target.name, e.target.value)} />
             <textarea className="h-32 col-span-2 bg-input-box-gray rounded 
             px-3 py-3 text-white" 
